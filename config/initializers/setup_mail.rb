0fbbbda563cf5d5a9c6cde935ca14ff9e597c572
Jack Clackett <jclackett@gmail.com>
2015-12-03 02:07:10 +0000

    updates ..... FFS

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
  :port                 => 587, 
  :domain               => "gmail.com", 
  :user_name => "jclackett",
- :password => "onion123",
+ :password => "/////////",
  :authentication       => "plain", 
  :enable_starttls_auto => true
 }
